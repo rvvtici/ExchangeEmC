9dd48ff612bb1435fafcad9112c12a17c79b7e1c
claracinci <cincinato1109@gmail.com>
2024-10-01 17:17:51 -0300

    Atualizando main.c com o código

---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long cpf;
+int senha;
+int cpf_confirmado = 0;
+int senha_confirmada = 0;
+int linha_usuario = 0;
+
+int criar_usuarios (char *nome, long long cpf, int senha){
+ 
+}
+
+int usuarios_txt() {
+   
+}
+
+int conferirCPF(){ 
+  
+}
+
+int conferir_senha(){
+  
+}
+
+void menu(){
+    printf("---------------\n");
+    printf("1. Consultar saldo\n");
+    printf("2. Consultar extrato\n");
+    printf("3. Depositar fundos\n");
+    printf("4. Sacar fundos\n");
+    printf("5. Compra de criptomoedas\n");
+    printf("6. Venda de criptomoedas\n");
+    printf("7. Atualizar cotacoes\n");
+    printf("8. Menu\n");
+    printf("0. Sair\n");
+    printf("---------------\n");
+};
+
+int consultar_saldo(){
+ 
+}
+
+int depositar_fundos(){
+  
+}
+
+int sacar_fundos(){
+
+}
+
+int consultar_extrato(){
+   
+}
+
+int compra_criptomoedas(){
+
+}
+
+
+int criar_arquivo_cotacao(){
+ 
+}
+
+int ler_arquivo_cotacao() {
+    
+}
+
+void atualizar_moeda() {
+   
+}
+
+int main(){
+ 
+
+printf("Boas vindas ao Exchange de Criptomoedas.\nPor favor, insira seu CPF e senha.\n");
+}
